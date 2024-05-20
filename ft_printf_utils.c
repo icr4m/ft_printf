7257ba147408182cc448aba7ff9bc0b5d435d217
Icham Jaber <ijaber@student.42.fr>
2024-05-20 19:32:01 +0200

    update

---
+++ ft_printf_utils.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 17:03:37 by ijaber            #+#    #+#             */
+/*   Updated: 2024/05/20 19:31:57 by ijaber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_putchar_fd(int fd, char c)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putstr_fd(int fd, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		ft_putchar_fd(fd, &str[i++]);
+}
