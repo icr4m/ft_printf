703ed7515f2ca87461afa728f8e0a98652504861
Icham Jaber <ijaber@student.42.fr>
2024-05-23 14:04:37 +0200

    main_test_pixel

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ijaber <ijaber@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 16:41:49 by ijaber            #+#    #+#             */
-/*   Updated: 2024/05/23 13:41:26 by ijaber           ###   ########.fr       */
+/*   Updated: 2024/05/23 14:03:33 by ijaber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,19 +42,56 @@ int	ft_printf(char const *str, ...)
 
 int	main(void)
 {
-	const int	i = printf("PRINTF_OG:%c\n", 0x23);
-	const int	i2 = ft_printf("FT_PRINTF:%c\n", 0x23);
+	const char	*str1 = NULL;
+	int			i;
+	int			i2;
+	const char	*str2 = "test";
+	int			i3;
+	int			i4;
+	int			i5;
+	int			i6;
+	int			i7;
+	int			i8;
 
-	// const char	*str1 = NULL;
+	// test 1
+	printf(Purple "Test 1 :\n");
+	printf(White);
+	i = printf("PRINTF_OG:%s\n", str1);
+	i2 = ft_printf("FT_PRINTF:%s\n", str1);
 	if (i2 == i)
-	{
-		printf(Green);
-		printf("Masterclass");
-	}
+		printf(Green "Ok ✓");
 	else
-	{
-		printf(Red);
-		printf("Boooouh OG=%d, FT:%d", i, i2);
-	}
+		printf(Red "KO ✗ OG=%d, FT:%d", i, i2);
+	printf(White "\n");
+	// test 2
+	printf(Purple "Test 2 :\n");
+	printf(White);
+	i3 = printf("PRINTF_OG: %s %x %c\n", str2, -42, '>');
+	i4 = ft_printf("FT_PRINTF: %s %x %c\n", str2, -42, '>');
+	if (i3 == i4)
+		printf(Green "Ok ✓");
+	else
+		printf(Red "KO ✗ OG=%d, FT:%d", i3, i4);
+	printf(White "\n");
+	// test 3
+	printf(Purple "Test 3 :\n");
+	printf(White);
+	i5 = printf("PRINTF_OG: %d %X %u\n", -42, -42, -42);
+	i6 = ft_printf("FT_PRINTF: %d %X %u\n", -42, -42, -42);
+	if (i5 == i6)
+		printf(Green "Ok ✓");
+	else
+		printf(Red "KO ✗ OG=%d, FT:%d", i5, i6);
+	printf(White "\n");
+	// test 4
+	printf(Purple "Test 4 :\n");
+	printf(White);
+	i7 = printf("PRINTF_OG: %i %% %p\n", -42, &str2);
+	i8 = ft_printf("FT_PRINTF: %i %% %p\n", -42, &str2);
+	if (i7 == i8)
+		printf(Green "Ok ✓");
+	else
+		printf(Red "KO ✗ OG=%d, FT:%d", i7, i8);
+	printf(White "\n");
 	return (0);
 }
